0edc1d0e1b0b3694e125d5091d522be030fd5fee
Onore849 <rion3376@gmail.com>
2020-10-10 16:04:15 +0900

    first commit

--- Custom Indicator/ContentView.swift
+++ Custom Indicator/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Button(action: {
+                
+                self.show.toggle()
+                
+            }) {
+                
+                 Text("Custom Indicator")
+                
+            }
+            
+            if self.show {
+                
+                GeometryReader{ _ in
+                    
+                    Loader()
+                    
+                }.background(Color.black.opacity(0.45).edgesIgnoringSafeArea(.all))
+                .onTapGesture {
+                    self.show.toggle()
+                }
+            }
+        }
     }
 }
 
@@ -18,4 +43,35 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Loader : View {
+    
+    @State var animate = false
+    
+    var body : some View {
+        
+        VStack {
+            
+            Circle()
+                .trim(from: 0, to: 0.8)
+                .stroke(AngularGradient(gradient: .init(colors: [.red, .orange]), center: .center), style:
+                            StrokeStyle(lineWidth: 8, lineCap: .round))
+                .frame(width: 45, height: 45)
+                .rotationEffect(.init(degrees: self.animate ? 360 : 0))
+                .animation(Animation.linear(duration: 0.7).repeatForever(autoreverses: false))
+            
+            Text("Please wait ....").padding(.top)
+            
+        }
+        .padding(20)
+        .background(Color.white)
+        .cornerRadius(15)
+        .onAppear {
+            self.animate.toggle()
+            
+        }
+
+    }
+
 }
