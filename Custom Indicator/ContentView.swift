dd0ad21731c15d786d3e0c4e977d1fa501912881
Onore849 <rion3376@gmail.com>
2020-10-10 15:36:06 +0900

    Initial Commit

---
+++ Custom Indicator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Indicator
+//
+//  Created by 野澤拓己 on 2020/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
